4c075c48ecaffee7bc51a686c26564f7fbff37e8
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-10-21 15:53:01 +0200

    Refactor rectangle drawing functions to utilize DrawRectangle for empty and solid rectangles

--- cmdline_drawing/rectangles/empty.go
+++ cmdline_drawing/rectangles/empty.go
@@ -4,18 +4,7 @@ package rectangles
 // Zeichnet ein Rechteck mit diesen Seitenlängen auf der Konsole.
 // Der Rand des Rechtecks soll aus `#`-Zeichen bestehen, der Innenraum soll leer sein.
 func DrawEmptyRectangle(height, width int) {
-	for row := 0; row < height; row++ {
-		for col := 0; col < width; col++ {
-			// Wenn wir uns am Rand befinden
-			//"#", sonst ""
-			if {
-			fmt.Print("#")
-			} else {
-				fmt.Print(" ")
-			}
-		}
-		fmt.Println()
-	}
+	DrawRectangle(height, width, " ", "#")
 
 }
 
--- cmdline_drawing/rectangles/solid.go
+++ cmdline_drawing/rectangles/solid.go
@@ -1,17 +1,8 @@
 package rectangles
 
-import "fmt"
-
 // Erwartet zwei Seitenlängen `height` und `width`.
 // Zeichnet ein Rechteck mit diesen Seitenlängen auf der Konsole.
 // Das Rechteck soll komplett mit `#`-Zeichen gefüllt sein.
 func DrawSolidRectangle(height, width int) {
-	for row := 0; row < height; row++ {
-
-		// Zeischne eine Zeile auf die Konsole
-		for col := 0; col < width; col++ {
-			fmt.Print("#")
-		}
-		fmt.Println()
-	}
+	DrawRectangle(height, width, "#", "#")
 }
