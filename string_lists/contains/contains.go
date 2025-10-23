d0db953dd3dde24dd24c296dde6614d70640e468
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-10-23 14:04:45 +0200

    Fix Contains function to correct syntax and improve readability

--- string_lists/contains/contains.go
+++ string_lists/contains/contains.go
@@ -3,10 +3,10 @@ package contains
 // Erwartet einen String und eine Liste von Strings.
 // Gibt true zurück, wenn der String in der Liste enthalten ist, ansonsten false.
 func Contains(l []string, x string) bool {
-	for i:=0; i<len(l); i++ {
-		l[i] == x {
+	for i := 0; i < len(l); i++ {
+		if l[i] == x {
 			return true
-		
+
 		}
 	}
 	return false
