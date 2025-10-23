5eac1123ce2c01ae7cd7c03d4bc026d53fccf2e5
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-10-23 14:04:33 +0200

    Fix Contains function to correctly check for string presence in a list

--- string_lists/contains/contains.go
+++ string_lists/contains/contains.go
@@ -2,7 +2,12 @@ package contains
 
 // Erwartet einen String und eine Liste von Strings.
 // Gibt true zurück, wenn der String in der Liste enthalten ist, ansonsten false.
-func Contains(strings []string, search string) bool {
-	// TODO
+func Contains(l []string, x string) bool {
+	for i:=0; i<len(l); i++ {
+		l[i] == x {
+			return true
+		
+		}
+	}
 	return false
 }
